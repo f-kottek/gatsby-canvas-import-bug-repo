2a75a9b956ad5221553dc2bab8ada6db428c2fb7
Frithjof Kottek <f.kottek@bi-medien.de>
2023-09-06 08:50:53 +0200

    bug setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,5 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+  // plugins: [`gatsby-plugin-sharp`],
+};
---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+const { createCanvas } = require("canvas");
+
+console.log(createCanvas);
+
+const img_canvas = createCanvas(950, 600);
+
+console.log(img_canvas);
